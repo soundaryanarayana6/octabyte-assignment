bc6d63f7a80a0ce6030f17d77d47563bc416e695
soundaryanarayana6 <soundaryan2063@gmail.com>
2025-12-13 21:04:06 +0530

    fix issues

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket         = "octabyte-tf-state-20251213152857404500000001"
+    region         = "us-east-1"
+    dynamodb_table = "octabyte-tf-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
